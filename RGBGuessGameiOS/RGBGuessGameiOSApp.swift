97e6eacef6db58448d16c3129f8eb64bf7852e40
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-06 21:34:52 +0200

    Initial Commit

---
+++ RGBGuessGameiOS/RGBGuessGameiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBGuessGameiOSApp.swift
+//  RGBGuessGameiOS
+//
+//  Created by Damian Ogórek on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct RGBGuessGameiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
