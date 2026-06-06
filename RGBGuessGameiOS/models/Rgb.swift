e233c116d9d7ab83d6eaef4e18feb4d16b661b09
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-06 21:46:26 +0200

    feature(models): cretae model that represents Rgb values (red, green, blue)

---
+++ RGBGuessGameiOS/models/Rgb.swift
@@ -0,0 +1,14 @@
+//
+//  Rgb.swift
+//  RGBGuessGameiOS
+//
+//  Created by Damian Ogórek on 06/06/2026.
+//
+
+import Foundation
+
+struct Rgb {
+    var red = 0.0
+    var green = 0.0
+    var blue = 0.0
+}
