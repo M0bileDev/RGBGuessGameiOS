d0087aa0d75b8bc1ab3af863b3745b2fed405c30
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-07 20:52:33 +0200

    feature(views): add slider that represents one of the colors
    Change default color values

--- RGBGuessGameiOS/views/ContentView.swift
+++ RGBGuessGameiOS/views/ContentView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Color(red: 0.0, green: 0.0, blue: 0.0)
+            Color(red: 0.5, green: 0.5, blue: 0.5)
             Text("R: ??? G: ??? B: ???")
                 .padding()
-            Color(red: 0.0, green: 0.0, blue: 0.0)
+            Color(red: 0.5, green: 0.5, blue: 0.5)
             Text("R: ??? G: ??? B: ???")
                 .padding()
+            Slider(value: .constant(0.5))
             Button("Hit me!") {
                 //todo: add checkaction logic
             }
